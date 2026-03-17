f3de61f57a0e48456d6b6ab9154e7ce70534173d
Byoku <coyunesbyorne@gmail.com>
2026-03-17 18:26:04 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function openTab(evt, tabName) {
+    var i, content, tabs;
+    content = document.getElementsByClassName("tab-content");
+    for (i = 0; i < content.length; i++) { 
+        content[i].classList.remove("active"); 
+    }
+    tabs = document.getElementsByClassName("tab-btn");
+    for (i = 0; i < tabs.length; i++) { 
+        tabs[i].classList.remove("active"); 
+    }
+    document.getElementById(tabName).classList.add("active");
+    evt.currentTarget.classList.add("active");
+}
