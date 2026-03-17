f623c88ae90aeb238717073de7a1015ddba678bd
Byoku <coyunesbyorne@gmail.com>
2026-03-17 18:34:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 function openTab(evt, tabName) {
     var i, content, tabs;
+    
+    // Hide all tab content
     content = document.getElementsByClassName("tab-content");
-    for (i = 0; i < content.length; i++) { 
-        content[i].classList.remove("active"); 
+    for (i = 0; i < content.length; i++) {
+        content[i].classList.remove("active");
     }
+    
+    // Deactivate all buttons
     tabs = document.getElementsByClassName("tab-btn");
-    for (i = 0; i < tabs.length; i++) { 
-        tabs[i].classList.remove("active"); 
+    for (i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove("active");
     }
+    
+    // Show the current tab and add an "active" class to the button
     document.getElementById(tabName).classList.add("active");
     evt.currentTarget.classList.add("active");
 }
